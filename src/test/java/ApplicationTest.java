b6a7fe8c5c93f2b1f02c67aecdb7e833a90d83f0
zhumengyifang <wangjiayuzh@163.com>
2019-03-15 22:14:44 +0800

    banner

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -17,7 +17,7 @@ public class ApplicationTest {
     private MockMvc mvc;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
     }
 
